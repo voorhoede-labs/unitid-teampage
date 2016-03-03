263e64bafb96e6c40e9a8d66530e9a0efa195489
Anne Fortuin <phortuin@gmail.com>
2016-03-03 15:46:19 +0100

    poc for seek to x and play for y secs

--- js.js
+++ js.js
@@ -2,32 +2,57 @@
 
     var vid = document.getElementById('vid');
     var video_button = document.getElementById('video_button');
+        var didit = false;
 
     var vid_thing = {
 
       init: function() {
+        vid.classList.add('visible');
           return this;
       },
 
       run: function() {
-          this.playForSomeTime(1);
-          vid.classList.add('playing');
+
+            this.goToPointInVideo(8);
+            this.playForSomeTime(1);
+          this.attachButtonEvent();
           return this;
       },
 
+      attachButtonEvent: function() {
+          video_button.addEventListener('click', function(event) {
+            if (this.is_playing) {
+                clearTimeout(this.timeout);
+                vid.pause();
+            } else {
+                vid.play();
+            }
+          }.bind(this));
+      },
+
+      goToPointInVideo: function(seconds) {
+        vid.pause();
+        vid.currentTime = seconds;
+        return this;
+      },
+
       playForSomeTime: function(seconds) {
-        var timeout;
         vid.pause();
         vid.play();
-        timeout = setTimeout(function() {
+        this.is_playing = true;
+        this.timeout = setTimeout(function() {
             vid.pause();
-        }, seconds * 1000);
+            this.is_playing = false;
+        }.bind(this), seconds * 1000);
       },
     }
 
     vid.addEventListener('canplay', function() {
-      videoo = vid_thing.init();
-      videoo.run();
+        if (!didit) {
+            didit = true;
+            videoo = vid_thing.init();
+            videoo.run();
+        }
     });
 
 }());
