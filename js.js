865ec3b6672ad816254f7df51c9ad67a14e8172e
Anne Fortuin <phortuin@gmail.com>
2016-03-03 15:16:02 +0100

    some workings

--- js.js
+++ js.js
@@ -1,20 +1,33 @@
 (function() {
 
     var vid = document.getElementById('vid');
+    var video_button = document.getElementById('video_button');
+
     var vid_thing = {
 
       init: function() {
-          console.log('running');
           return this;
       },
 
       run: function() {
+          this.playForSomeTime(1);
+          vid.classList.add('playing');
           return this;
-      }
+      },
+
+      playForSomeTime: function(seconds) {
+        var timeout;
+        vid.pause();
+        vid.play();
+        timeout = setTimeout(function() {
+            vid.pause();
+        }, seconds * 1000);
+      },
     }
 
     vid.addEventListener('canplay', function() {
-      vid_thing.init();
+      videoo = vid_thing.init();
+      videoo.run();
     });
 
 }());
