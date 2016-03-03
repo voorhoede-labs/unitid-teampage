6dfcdf688280361e127d325a2681c0eb5f373eff
Anne Fortuin <phortuin@gmail.com>
2016-03-03 15:05:47 +0100

    add basic event listener for vid

--- js.js
+++ js.js
@@ -1,5 +1,20 @@
 (function() {
 
-  var vid = doc.getElementById('vid')
+    var vid = document.getElementById('vid');
+    var vid_thing = {
+
+      init: function() {
+          console.log('running');
+          return this;
+      },
+
+      run: function() {
+          return this;
+      }
+    }
+
+    vid.addEventListener('canplay', function() {
+      vid_thing.init();
+    });
 
 }());
